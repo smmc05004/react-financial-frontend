6fe647b32ff07c5ae3807f203e46dc152edacbfc
SEOKHWALEE <smmc05004@hanmail.net>
2019-12-24 15:07:36 +0900

    * 가계부 버튼 색상 변경

--- src/components/ledger/CrudBtnBundle.js
+++ src/components/ledger/CrudBtnBundle.js
@@ -4,13 +4,13 @@ import { Button } from 'reactstrap';
 const CrudBtnBundle = ({ onInsert, onUpdate, onDelete }) => {
   return (
     <div className="btnBundle">
-      <Button onClick={onInsert} color="success" size="sm">
+      <Button onClick={onInsert} color="primary" size="sm">
         입력
       </Button>
-      <Button onClick={onUpdate} color="primary" size="sm">
+      <Button onClick={onUpdate} color="success" size="sm">
         수정
       </Button>
-      <Button onClick={onDelete} color="warning" size="sm">
+      <Button onClick={onDelete} color="danger" size="sm">
         삭제
       </Button>
     </div>
